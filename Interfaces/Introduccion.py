669f7dad8044cacf142e0ee980cb9f73484c6673
Cerino-rigo <cerino.rigoberto@tec.mx>
2023-10-31 10:23:44 -0600

    Agregué función suma y creé archivo modificación

--- Interfaces/Introduccion.py
+++ Interfaces/Introduccion.py
@@ -1,1 +1,8 @@
 print("Hola mundo, hoy crearemos una interfaz")
+
+a=50
+b=45
+def suma():
+    return print(a + b)
+
+suma()
---
+++ Interfaces/Modificacion.py
@@ -0,0 +1,3 @@
+cambios = input("Introduce el nombre del cambio a realizar: ")
+
+print("El cambio realizado es el siguiente: ", cambios)
